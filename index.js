34450203636eb1408c08a907711801881852cb21
Denys Leonchev <den.leonchev@gmail.com>
2018-07-25 21:22:57 +0300

    Fix env variable name

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => { // eslint-disable-line no-unused-vars
     .send('Something terrible happened on server!');
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`The server is running on port ${port}`);
 });
