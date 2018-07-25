6bb986fd8249758ec8e28c076e270ed7ab2f6d87
Denys Leonchev <den.leonchev@gmail.com>
2018-07-25 21:48:36 +0300

    Fix reset password expiration key

--- src/config/prod.js
+++ src/config/prod.js
@@ -2,5 +2,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   secret: process.env.SECRET,
   sendgridKey: process.env.SENDGRID_KEY,
-  resetPasswordExpiration: process.env.RESET_PASSWORD_EXPIRATION,
+  resetPasswordExpiration: Number(process.env.RESET_PASSWORD_EXPIRATION),
 };
